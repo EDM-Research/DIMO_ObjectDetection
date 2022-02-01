093474eaf7cf448b2029d2ad35f083f013149d5c
VR/Render-pc-1 <steven.moonen@uhasselt.be>
2022-02-01 15:45:29 +0100

    Update requirements

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 from data import utils
+from data import mrcnn_dimo
+from training import mrcnn
+
+DIMO_PATH = "D:/Datasets/DIMO/dimo"
+
+
+def train_subsets(subsets):
+    train, val = mrcnn_dimo.get_dimo_datasets(DIMO_PATH, subsets)
+
+    print(f"training images: {len(train.image_ids)}")
+    print(f"validation images: {len(val.image_ids)}")
+
+    mrcnn.train(train, val, mrcnn_dimo.DimoConfig(), False)
+
+
+def prepare_subsets(subsets):
+    utils.create_dimo_masks(DIMO_PATH, subsets)
+    utils.create_dimo_train_split(DIMO_PATH, subsets, seed=10)
+
 
 if __name__ == "__main__":
-    utils.create_dimo_train_split("F:/Data/dimo", ["sim_jaigo", "real_jaigo"])
+    train_subsets(["real_jaigo_000-150"])
