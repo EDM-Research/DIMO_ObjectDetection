Fixed image sizing for map computation

training/evaluation.py
@@ -3,20 +3,43 @@ from mrcnn.config import Config
 import mrcnn.model as modellib
 import numpy as np
 from mrcnn import visualize
+import os
 
 
-def load_model(model_dir: str, config: Config) -> modellib.MaskRCNN:
-    model = modellib.MaskRCNN(mode="inference", config=config, model_dir=f"models/{model_dir}")
-    model.load_weights(model.find_last())
+def get_file_for_epoch(model_dir: str, epoch: int = None) -> str:
+    def get_epoch_no(file_name: str) -> int:
+        return int(file_name.split('.')[0].split('_')[-1])
+
+    last_epoch = 0
+    last_epoch_file = ""
+
+    for file in os.listdir(model_dir):
+        if file.endswith('.h5'):
+            current_epoch = get_epoch_no(file) - 1
+            if current_epoch > last_epoch:
+                last_epoch = current_epoch
+                last_epoch_file = file
+            if current_epoch == epoch:
+                return file
+
+    return last_epoch_file
+
+
+def load_model(model_id: str, config: Config, epoch: int = None) -> modellib.MaskRCNN:
+    model = modellib.MaskRCNN(mode="inference", config=config, model_dir=f"/models")
+    model_file = get_file_for_epoch(f"models/{model_id}", epoch)
+    model_path = f"models/{model_id}/{model_file}"
+    print(f"Loading model from {model_path}")
+    model.load_weights(model_path, by_name=True)
     return model
 
 
-def get_detections(dataset: Dataset, model: modellib.MaskRCNN) -> list:
+def get_detections(dataset: Dataset, model: modellib.MaskRCNN, config: Config) -> list:
     results = []
 
     for i, image_id in enumerate(dataset.image_ids):
         print(f"Testing image {i}/{len(dataset.image_ids)}", end='\r')
-        image = dataset.load_image(image_id)
+        image, *_ = modellib.load_image_gt(dataset, config, image_id, use_mini_mask=False)
         result = model.detect([image], verbose=0)[0]
         result['image_id'] = image_id
         results.append(result)
@@ -28,8 +51,7 @@ def compute_map(results: list, dataset: Dataset, config: Config, iou_threshold: 
     aps = []
 
     for result in results:
-        image, image_meta, gt_class_id, gt_bbox, gt_mask = modellib.load_image_gt(dataset, config, result['image_id'],
-                                                                                  use_mini_mask=False)
+        image, image_meta, gt_class_id, gt_bbox, gt_mask = modellib.load_image_gt(dataset, config, result['image_id'], use_mini_mask=False)
         ap, precisions, recalls, overlaps = compute_ap(
             gt_boxes=gt_bbox,
             gt_class_ids=gt_class_id,
@@ -46,7 +68,7 @@ def compute_map(results: list, dataset: Dataset, config: Config, iou_threshold: 
     return np.mean(aps)
 
 
-def show_results(results: list, dataset: Dataset):
+def show_results(results: list, dataset: Dataset, config: Config):
     for result in results:
-        image = dataset.load_image(result['image_id'])
+        image, image_meta, gt_class_id, gt_bbox, gt_mask = modellib.load_image_gt(dataset, config, result['image_id'], use_mini_mask=False)
         visualize.display_instances(image, result['rois'], result['masks'], result['class_ids'], dataset.class_names)